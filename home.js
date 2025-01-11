f7d47232b5772e638cc627e23c9ca3e09c9c6e8c
eshasingh05 <esingh37@gatech.edu>
2025-01-11 17:27:53 -0500

    changing names

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 const playButton = document.getElementById("start");
 
 playButton.addEventListener("click", () => {
-    window.location.href = "index.html"; // Replace with the actual path to your file
+    window.location.href = "game.html"; 
 });
